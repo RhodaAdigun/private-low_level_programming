221773a59d65c5b2597699267093699aba07f8f5
RhodaAdigun <106974772+RhodaAdigun@users.noreply.github.com>
2022-08-02 17:42:39 +0100

    defines a macro as an abbreviation for token

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -2,9 +2,10 @@
 #define MACRO_H
 
 /**
-*a header file that defines a macro named SIZE
-*as an abbreviation for the token 1024
-*/
+* File: 0-object_like_macro.h
+ * Description: Header file that defines a macro named
+ *  SIZE as an abbreviation for the token 1024.
+ */
 
 #define SIZE 1024
 
